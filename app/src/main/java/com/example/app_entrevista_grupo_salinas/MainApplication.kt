91179abb56add60aa0ffd2aa9100fe4524b2aeec
Leonardo Alapizco <leonarmga@gmail.com>
2021-10-01 10:51:27 -0600

    Dependency Injection Provider created.

---
+++ app/src/main/java/com/example/app_entrevista_grupo_salinas/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.example.app_entrevista_grupo_salinas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+
+}
