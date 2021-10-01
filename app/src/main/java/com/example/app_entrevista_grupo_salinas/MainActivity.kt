7b499ce1e558c2695f455b9d43852283bc8103bb
Leonardo Alapizco <leonarmga@gmail.com>
2021-10-01 14:41:30 -0600

    Main navigation implemented for home

--- app/src/main/java/com/example/app_entrevista_grupo_salinas/MainActivity.kt
+++ app/src/main/java/com/example/app_entrevista_grupo_salinas/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.app_entrevista_grupo_salinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_entrevista_grupo_salinas.databinding.ActivityMainBinding
+import com.example.app_entrevista_grupo_salinas.home.HomeFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), MainNavigation {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        launchHomeFragment()
     }
+
+    override fun launchHomeFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container_view, HomeFragment(), null)
+            .commit()
+    }
+
+    override fun launchMoviesFragment() {
+        TODO("Not yet implemented")
+    }
+
+    override fun launchShowsFragment() {
+        TODO("Not yet implemented")
+    }
+
+    override fun launchMovieDetailFragment(movieId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun launchShowDetailFragment(showId: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/example/app_entrevista_grupo_salinas/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/app_entrevista_grupo_salinas/home/viewmodel/HomeViewModel.kt
@@ -2,7 +2,10 @@ package com.example.app_entrevista_grupo_salinas.home.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : ViewModel() {
+class HomeViewModel @Inject constructor(
+
+) : ViewModel() {
 }
